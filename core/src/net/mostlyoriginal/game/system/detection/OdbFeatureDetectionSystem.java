8a822c93299b590a4d500d7ac63b43a4383959a0
DaanVanYperen <daan@mostlyoriginal.net>
2015-02-21 15:56:41 +0100

    Remove ad-hoc coloring.

--- core/src/net/mostlyoriginal/game/system/detection/OdbFeatureDetectionSystem.java
+++ core/src/net/mostlyoriginal/game/system/detection/OdbFeatureDetectionSystem.java
@@ -7,7 +7,6 @@ import com.artemis.utils.EntityBuilder;
 import com.artemis.utils.reflect.ClassReflection;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import net.mostlyoriginal.game.component.detection.OdbFeatureComponent;
 import net.mostlyoriginal.game.component.detection.PackDetectionComponent;
 import net.mostlyoriginal.game.component.detection.PoolDetectionComponent;
@@ -75,10 +74,7 @@ public class OdbFeatureDetectionSystem extends EntityProcessingSystem {
 
 	@Override
 	protected void process(Entity e) {
-
 		// just for quick debugging on macos.
 		OdbFeatureComponent feature = e.getComponent(OdbFeatureComponent.class);
-		Gdx.gl.glClearColor(feature.isPooled ? 0 : 1, feature.isPooled ? 1 : 0, 0.25f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 }
--- core/src/net/mostlyoriginal/game/system/render/ClearScreenSystem.java
+++ core/src/net/mostlyoriginal/game/system/render/ClearScreenSystem.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.graphics.GL20;
 public class ClearScreenSystem extends VoidEntitySystem {
 	@Override
 	protected void processSystem() {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 }
