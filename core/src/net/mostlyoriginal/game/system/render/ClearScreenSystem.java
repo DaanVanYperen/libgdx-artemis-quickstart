997ba4991ffa4927eee9013d15b643ad556b8911
DaanVanYperen <daan@mostlyoriginal.net>
2015-02-21 16:39:57 +0100

    Color configuration to clear screen.

--- core/src/net/mostlyoriginal/game/system/render/ClearScreenSystem.java
+++ core/src/net/mostlyoriginal/game/system/render/ClearScreenSystem.java
@@ -2,17 +2,29 @@ package net.mostlyoriginal.game.system.render;
 
 import com.artemis.systems.VoidEntitySystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 
 /**
- * Responsible for clearing the screen.
+ * Clearing the screen.
  *
  * @author Daan van Yperen
  */
 public class ClearScreenSystem extends VoidEntitySystem {
+
+	private final Color color;
+
+	public ClearScreenSystem() {
+		this(Color.BLACK);
+	}
+
+	public ClearScreenSystem(Color color) {
+		this.color = color;
+	}
+
 	@Override
-	protected void processSystem() {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+	protected void processSystem( ) {
+		Gdx.gl.glClearColor(color.r, color.g,color.b,color.a);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 }
